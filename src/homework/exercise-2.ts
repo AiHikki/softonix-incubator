7407854ce379b8bf80069ae313809abc908f83cc
ai_hikki <freeomareps@gmail.com>
2024-09-13 16:51:03 +0300

    do hw

--- src/homework/exercise-2.ts
+++ src/homework/exercise-2.ts
@@ -1,4 +1,3 @@
-
 /*
   Опишіть тип TPerson на основі IUser та IAdmin та використовуйте це в масиві persons і функції logPerson,
   щоб виправити всі помилки TS.
@@ -16,9 +15,9 @@ interface IAdmin {
   role: string
 }
 
-type TPerson = unknown
+type TPerson = IUser | IAdmin
 
-const persons: IUser[] /* <- замінити на IPerson[] */ = [
+const persons: TPerson[] = [
   {
     name: 'Max Mustermann',
     age: 25,
@@ -41,8 +40,14 @@ const persons: IUser[] /* <- замінити на IPerson[] */ = [
   }
 ]
 
-function logPerson (user: IUser) {
-  console.log(` - ${user.name}, ${user.age}`)
+function logPerson (person: TPerson) {
+  console.log(` - ${person.name}, ${person.age}`)
+
+  if ('occupation' in person) {
+    console.log(`Occupation: ${person.occupation}`)
+  } else if ('role' in person) {
+    console.log(`Role: ${person.role}`)
+  }
 }
 
 export {
